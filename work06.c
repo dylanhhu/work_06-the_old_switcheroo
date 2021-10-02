57e92533ad8ff262fc40ccde04eb7c04d211eb69
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2021-10-02 14:20:52 -0400

    finished problem

--- work06.c
+++ work06.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("hello world\n");
+    int original_array[10];
+    original_array[0] = 0;
+
+    // seed the RNG
+    srand(time(NULL));
+
+    // fill the rest randomly
+    int i;
+    for (i = 1; i < 10; i++) {
+        original_array[i] = rand();
+    }
+
+    // print out the array
+    printf("The Original Array:\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d: %d\n", i, original_array[i]);
+    }
+
+    int new_array[10];
+    int *original_ptr = original_array;
+    int *new_ptr = new_array;
+
+    // fill second new array with the reverse of
+    // the original; only using pointers
+    for (i = 0; i < 10; i++) {
+        *(new_ptr + 9 - i) = original_ptr[i];
+    }
+
+    // print the new array
+    printf("\nThe New Reversed Array:\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d: %d\n", i, new_array[i]);
+    }
 
     return 0;
 }
